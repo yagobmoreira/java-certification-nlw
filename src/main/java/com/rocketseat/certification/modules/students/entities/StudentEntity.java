7f259f4046485132d5861e605e7c5ec9c4974464
Yago Moreira <yagomoreira@gmail.com>
2024-06-24 11:04:12 -0300

    feat: add database

--- src/main/java/com/rocketseat/certification/modules/students/entities/StudentEntity.java
+++ src/main/java/com/rocketseat/certification/modules/students/entities/StudentEntity.java
@@ -1,18 +1,35 @@
 package com.rocketseat.certification.modules.students.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity(name = "students")
 public class StudentEntity {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Column(unique = true, nullable = false)
   private String email;
+
+  @OneToMany(mappedBy = "studentEntity") //nome do atributo
   private List<CertificationStudentEntity> certificationStudentEntities;
 
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 }
